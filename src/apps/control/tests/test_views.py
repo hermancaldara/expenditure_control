43b8f76f2d7f590b1f7db309337c4e54c0f01884
Herman Caldara <hermancaldara@gmail.com>
2011-07-31 15:20:38 -0300

    Added tests of submit a expense

--- src/apps/control/tests/test_views.py
+++ src/apps/control/tests/test_views.py
@@ -1,10 +1,29 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 from should_dsl import should
+
+from apps.control.views import index
 
 
 class ControlViewsTestCase(TestCase):
 
+    def setUp(self):
+        self.client = Client()
+
     def test_index(self):
         response = self.client.get('/')
         response.status_code |should| equal_to(200)
+        response.context.has_key('form') |should| be(True)
+
+    def test_submit_a_expense(self):
+        response = self.client.post('/', 
+            {'value': 10.0, 'category': 'food', 'description': 'Rice', 'date': '01/01/2011'}
+        )
+        response.status_code |should| equal_to(302)
+
+    def test_submit_a_expense_with_error(self):
+        response = self.client.post('/', 
+            {'category': 'food', 'description': 'Rice', 'date': '01/01/2011'}
+        )
+        form = response.context.get('form')
+        form.errors['value'] |should| equal_to([u'This field is required.'])
--- src/apps/control/views.py
+++ src/apps/control/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.http import HttpResponseRedirect
+
+from apps.control.forms import ExpenseForm
 
 
 def index(request):
+    if request.method == 'POST':
+        form = ExpenseForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    else:
+        form = ExpenseForm()
+
     return render_to_response(
         'index.html',
-        {},
+        {'form': form},
         context_instance=RequestContext(request)
     )
